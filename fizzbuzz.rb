d68e1abafe48bced273de216b31445e33d6cc076
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-21 18:51:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,10 @@
 
 def fizzbuzz(int)
   if int % 3 == 0
-    return "Fizz"
+     "Fizz"
   elsif int % 5 == 0
-    return "Buzz"
+     "Buzz"
   elsif (int % 5 == 0) && (int % 3 == 0)
-    return "FizzBuzz"  
+     "FizzBuzz"  
 end
 end
